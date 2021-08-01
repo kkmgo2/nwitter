3e8769fe66ba310920aa175eb0c4e41ea7f8f928
Kyungmo Kim <kyungmo.kim@autodesk.com>
2021-08-01 11:49:08 +1000

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEvIw0xFC3u1rLBVYfWXxZH0hRvkgiBhs",
+    authDomain: "nwitter-c45d0.firebaseapp.com",
+    projectId: "nwitter-c45d0",
+    storageBucket: "nwitter-c45d0.appspot.com",
+    messagingSenderId: "12540603504",
+    appId: "1:12540603504:web:87f17814cc08768e89d8b5"
+  };
+
+export default firebase.initializeApp(firebaseConfig);

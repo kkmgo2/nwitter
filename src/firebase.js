58ce7c4f3bba48ad7ced273ca5df7f095af3bcd9
Kyungmo Kim <kyungmo.kim@autodesk.com>
2021-08-02 17:58:14 +1000

    move credentials to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEvIw0xFC3u1rLBVYfWXxZH0hRvkgiBhs",
-    authDomain: "nwitter-c45d0.firebaseapp.com",
-    projectId: "nwitter-c45d0",
-    storageBucket: "nwitter-c45d0.appspot.com",
-    messagingSenderId: "12540603504",
-    appId: "1:12540603504:web:87f17814cc08768e89d8b5"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 export default firebase.initializeApp(firebaseConfig);
